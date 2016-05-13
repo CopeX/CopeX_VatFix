d95a7776ba345f3df2487dc469b6d26c68a5f49d
Roman Hutterer <roman.hutterer@copex.io>
2016-05-13 16:54:24 +0200

    fix of registration.php file to avoid compiling errors

--- registration.php
+++ registration.php
@@ -17,7 +17,8 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-$name = implode('_', array_map(function ($part) {
-    return implode(array_map('ucfirst', explode('-', $part)));
-}, array_slice(explode(DIRECTORY_SEPARATOR, __DIR__), -2, 2)));
-ComponentRegistrar::register(ComponentRegistrar::MODULE, $name, __DIR__);
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CopeX_VATFix',
+    __DIR__
+);
